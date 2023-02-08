09a99abf62d8370e31004207cf688c7df5bf99a2
Angie <angelicareutersward@hotmail.com>
2023-02-08 16:47:29 +0100

    Social media icons, test av ts createHtml

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,20 +1,47 @@
-function init() {
-  createHtml();
-}
-
 function createHtml() {
-  //hämta container
-  let main = document.getElementById("main-container") as HTMLDivElement;
+  let mainContainer: HTMLDivElement = document.getElementById(
+    "mainContainer"
+  ) as HTMLDivElement;
 
-  //skapa element för att kunna animatea i scss
-  let heading = document.createElement("div");
-  heading.innerHTML = "<h1>Welcome to my portfolio!</h1>";
-  let heartContainer = document.createElement("div");
-  let heartAnimation = document.createElement("div");
-  heartContainer.classList.add("heart-container");
-  heartAnimation.classList.add("heart");
-  heartContainer.appendChild(heartAnimation);
-  main.appendChild(heading);
-  main.appendChild(heartContainer);
+  let aboutContainer = document.createElement("div");
+  let projectsContainer = document.createElement("div");
+  let contactContainer = document.createElement("div");
+
+  aboutContainer.classList.add("shadow-sm p-3 mb-5 bg-body rounded");
+  projectsContainer.classList.add("shadow-sm p-3 mb-5 bg-body rounded");
+  contactContainer.classList.add("shadow-sm p-3 mb-5 bg-body rounded");
+
+  //aboutContainer
+  let heading = document.createElement("h4");
+  heading.innerHTML = "Hi! I'm Angie!";
+
+  let paragraph1 = document.createElement("p");
+  paragraph1.innerHTML = "Welcome to my portfolio.";
+
+  let paragraph2 = document.createElement("div");
+  let span1 = document.createElement("span");
+  let span2 = document.createElement("span");
+  let span3 = document.createElement("span");
+  span1.innerHTML = "I love building things and seeing them come ";
+  span2.innerHTML = "alive";
+  span3.innerHTML = "!";
+  span2.classList.add("alive");
+  paragraph2.appendChild(span1);
+  paragraph2.appendChild(span2);
+  paragraph2.appendChild(span3);
+
+  aboutContainer.appendChild(heading);
+  aboutContainer.appendChild(paragraph1);
+  aboutContainer.appendChild(paragraph2);
+
+  //projectsContainer
+
+  //contactContainer
+
+  //append
+  mainContainer.appendChild(aboutContainer);
+  mainContainer.appendChild(projectsContainer);
+  mainContainer.appendChild(contactContainer);
 }
-init();
+
+createHtml();
