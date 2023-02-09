e2d8eab3596b9a0221b558c150553b398fe0a153
Angie <angelicareutersward@hotmail.com>
2023-02-09 21:21:08 +0100

    portfolio update

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,24 +1,30 @@
+import { url } from "inspector";
+
 function createHtml() {
-  let mainContainer: HTMLDivElement = document.getElementById(
-    "mainContainer"
+  let helloContainer: HTMLDivElement = document.getElementById(
+    "helloContainer"
+  ) as HTMLDivElement;
+  let aboutContainer: HTMLDivElement = document.getElementById(
+    "aboutContainer"
+  ) as HTMLDivElement;
+  let techStackContainer: HTMLDivElement = document.getElementById(
+    "techStack"
+  ) as HTMLDivElement;
+  let projectsContainer: HTMLDivElement = document.getElementById(
+    "projectsContainer"
+  ) as HTMLDivElement;
+  let contactContainer: HTMLDivElement = document.getElementById(
+    "contactContainer"
   ) as HTMLDivElement;
 
-  let aboutContainer = document.createElement("div");
-  let projectsContainer = document.createElement("div");
-  let contactContainer = document.createElement("div");
+  //helloContainer
+  let helloHeading = document.createElement("h4");
+  helloHeading.innerHTML = "Hi! I'm Angie!";
 
-  aboutContainer.classList.add("shadow-sm p-3 mb-5 bg-body rounded");
-  projectsContainer.classList.add("shadow-sm p-3 mb-5 bg-body rounded");
-  contactContainer.classList.add("shadow-sm p-3 mb-5 bg-body rounded");
+  let helloParagraph1 = document.createElement("p");
+  helloParagraph1.innerHTML = "Welcome to my portfolio.";
 
-  //aboutContainer
-  let heading = document.createElement("h4");
-  heading.innerHTML = "Hi! I'm Angie!";
-
-  let paragraph1 = document.createElement("p");
-  paragraph1.innerHTML = "Welcome to my portfolio.";
-
-  let paragraph2 = document.createElement("div");
+  let helloParagraph2 = document.createElement("div");
   let span1 = document.createElement("span");
   let span2 = document.createElement("span");
   let span3 = document.createElement("span");
@@ -26,22 +32,85 @@ function createHtml() {
   span2.innerHTML = "alive";
   span3.innerHTML = "!";
   span2.classList.add("alive");
-  paragraph2.appendChild(span1);
-  paragraph2.appendChild(span2);
-  paragraph2.appendChild(span3);
+  helloParagraph2.appendChild(span1);
+  helloParagraph2.appendChild(span2);
+  helloParagraph2.appendChild(span3);
 
-  aboutContainer.appendChild(heading);
-  aboutContainer.appendChild(paragraph1);
-  aboutContainer.appendChild(paragraph2);
+  //let profilePic = document.createElement("img") as HTMLImageElement;
+  //profilePic.setAttribute("src", "/src/assets/profilepic.jpeg"); //VARFÖR BRUTEN BILD?!
+  //profilePic.setAttribute("alt", "Profile picture");
+  //profilePic.classList.add("profilePic");
+
+  helloContainer.appendChild(helloHeading);
+  helloContainer.appendChild(helloParagraph1);
+  helloContainer.appendChild(helloParagraph2);
+  //helloContainer.appendChild(profilePic);
+
+  //techStackContainer
+  // let techStackPic = document.createElement("img") as HTMLImageElement;
+  // techStackPic.setAttribute("src", "/src/assets/busybee.JPG"); //VARFÖR BRUTEN BILD?!
+  // techStackPic.setAttribute("alt", "Tech stack");
+  // techStackPic.classList.add("techStack");
+
+  // techStackContainer.appendChild(techStackPic);
+
+  //aboutContainer
+  let aboutHeading = document.createElement("h4");
+  aboutHeading.innerHTML = "About me";
+
+  let container = document.createElement("div");
+
+  let relevantEducation = document.createElement("div");
+  relevantEducation.classList.add(
+    "shadow",
+    "p-3",
+    "mb-5",
+    "bg-body",
+    "rounded"
+  );
+  let eduHeading = document.createElement("h5");
+  eduHeading.innerHTML = "Relevant education";
+  let eduParagraph1 = document.createElement("p");
+  eduParagraph1.innerHTML = "Coming soon...";
+  relevantEducation.appendChild(eduHeading);
+  relevantEducation.appendChild(eduParagraph1);
+  container.appendChild(relevantEducation);
+
+  let workExperience = document.createElement("div");
+  workExperience.classList.add("shadow", "p-3", "mb-5", "bg-body", "rounded");
+  let workHeading = document.createElement("h5");
+  workHeading.innerHTML = "Work experience";
+  let workParagraph1 = document.createElement("p");
+  workParagraph1.innerHTML = "Coming soon...";
+  workExperience.appendChild(workHeading);
+  workExperience.appendChild(workParagraph1);
+  container.appendChild(workExperience);
+
+  aboutContainer.appendChild(aboutHeading);
+  aboutContainer.appendChild(container);
 
   //projectsContainer
+  let projectsHeading = document.createElement("h4");
+  projectsHeading.innerHTML = "My projects";
+  let projParagraph1 = document.createElement("p");
+  projParagraph1.innerHTML = "Coming soon...";
+  projectsContainer.appendChild(projectsHeading);
+  projectsContainer.appendChild(projParagraph1);
 
   //contactContainer
+  let contactsHeading = document.createElement("h4");
+  contactsHeading.innerHTML = "Contact";
 
-  //append
-  mainContainer.appendChild(aboutContainer);
-  mainContainer.appendChild(projectsContainer);
-  mainContainer.appendChild(contactContainer);
+  let contactParagraph1 = document.createElement("p");
+  contactParagraph1.innerHTML =
+    "I am currently looking for an internship for fall of 2023. Feel free to contact me if you have openings!";
+
+  let contactParagraph2 = document.createElement("p");
+  contactParagraph2.innerHTML = "Find me via my socials below!";
+
+  contactContainer.appendChild(contactsHeading);
+  contactContainer.appendChild(contactParagraph1);
+  contactContainer.appendChild(contactParagraph2);
 }
 
 createHtml();
