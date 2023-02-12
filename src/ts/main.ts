eac1f9df4e0eee97aa08eedb8516baf3383b99b4
Angie <angelicareutersward@hotmail.com>
2023-02-12 14:48:21 +0100

    Tech skills added

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -145,28 +145,55 @@ function aboutContainer() {
   let aboutHeading = document.createElement("h4");
   aboutHeading.innerHTML = "About me";
 
-  let container = document.createElement("div");
+  let container = document.createElement("aboutBoxes");
   container.classList.add("aboutBoxes");
 
   //Tech Stack
-  let techStack = document.createElement("div");
-  techStack.classList.add("shadow-sm", "p-3", "mb-5", "bg-body", "rounded");
-  techStack.id = "techStackBox";
+  let techSkills = document.createElement("div");
+  techSkills.classList.add("shadow-sm", "p-3", "mb-5", "bg-body", "rounded");
+  techSkills.id = "techSkillsBox";
 
   let techHeading = document.createElement("h5");
-  techHeading.innerHTML = "Tech Stack";
+  techHeading.innerHTML = "Technical Skills";
 
-  let techImgBox = document.createElement("div");
-  techImgBox.classList.add("techImgBox");
-  let techImg = document.createElement("img");
-  techImg.src = "https://angelicareutersward.se/assets/ComingSoon.jpg";
-  techImg.alt = "Tech stack / Turtle";
-  techImg.classList.add("techImg");
-  techImgBox.appendChild(techImg);
+  // let techImgBox = document.createElement("div");
+  // techImgBox.classList.add("techImgBox");
+  // let techImg = document.createElement("img");
+  // techImg.src = "https://angelicareutersward.se/assets/ComingSoon.jpg";
+  // techImg.alt = "Tech stack / Turtle";
+  // techImg.classList.add("techImg");
+  // techImgBox.appendChild(techImg);
 
-  techStack.appendChild(techHeading);
-  techStack.appendChild(techImgBox);
-  container.appendChild(techStack);
+  let skillsIconsBox = document.createElement("div");
+  skillsIconsBox.classList.add("skillsIconsBox");
+  skillsIconsBox.innerHTML = `
+  <i class="devicon-typescript-plain">TypeScript </i>
+  <i class="devicon-javascript-plain">JavaScript </i>
+  <i class="devicon-react-original">React.js </i>
+  <i class="devicon-vuejs-plain">Vue.js </i>
+  <i class="devicon-html5-plain-wordmark">HTML5 </i>
+  <i class="devicon-sass-original">SCSS </i>
+  <i class="devicon-css3-plain">CSS3 </i>
+  <i class="devicon-bootstrap-plain">Bootstrap </i>
+  <span>Parcel.js </span>
+  <span>Test automation </span>
+  <i class="devicon-jest-plain">Jest</i>
+  <span>Cypress </span>
+  <i class="devicon-git-plain">Git </i>
+  <i class="devicon-github-original">GitHub </i>
+  <i class="devicon-nodejs-plain">Node.js </i>
+  <i class="devicon-graphql-plain">GraphQL </i>
+  <span>REST-Api </span>
+  <i class="devicon-mongodb-plain">MongoDB </i>
+  <span>NoSQL </span>
+  <span>SQL </span>
+  `;
+
+  techSkills.appendChild(techHeading);
+  // techSkills.appendChild(techImgBox);
+  techSkills.appendChild(skillsIconsBox);
+
+  container.appendChild(techSkills);
 
   //Relevant Education
   let relevantEducation = document.createElement("div");
