efdc840bbccada9b57385fdc8182a9e76cf739cc
Ayngie <angelicareutersward@gmail.com>
2023-03-29 18:52:25 +0200

    ♻️ (refactor): changed url for image

--- src/ts/functions/helloContainer/createProfileImage.ts
+++ src/ts/functions/helloContainer/createProfileImage.ts
@@ -3,8 +3,8 @@ export function createProfileImage(profileImgBox: HTMLDivElement): HTMLDivElemen
 
   const profileImg = document.createElement("img");
   // profileImg.src = "../../../assets/ProfilePic.jpeg";
-  // profileImg.src = "https://angelicareutersward.se/Images/ProfilePic.jpeg";
-  profileImg.src = "  https://raw.githubusercontent.com/Ayngie/portfolio2.0/main/src/assets/ProfilePic.jpeg";
+  profileImg.src = "https://angelicareutersward.se/Images/ProfilePic.jpeg";
+  // profileImg.src = "  https://raw.githubusercontent.com/Ayngie/portfolio2.0/main/src/assets/ProfilePic.jpeg";
   profileImg.alt = "Profile picture";
   profileImg.classList.add("profileImg");
   profileImgBox.appendChild(profileImg);
