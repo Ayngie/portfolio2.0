601c9920c3cd7cc4d560a567f1de60f6afede30c
Angie <71646930+Ayngie@users.noreply.github.com>
2024-04-30 16:09:14 +0200

    Commented out temporarily unused code - modifyIndividualProject.ts

--- src/ts/functions/projectsContainer/modifyIndividualProject.ts
+++ src/ts/functions/projectsContainer/modifyIndividualProject.ts
@@ -4,18 +4,18 @@ export function modifyIndividualProject(
   receivedRepo: IRepo,
   title: HTMLElement,
   description: HTMLElement,
-  imgLink: HTMLAnchorElement,
-  backgroundImgContainer: HTMLElement,
-  tryItLink: HTMLAnchorElement,
-  gitHubLink: HTMLAnchorElement,
-  rowContainer: HTMLElement
+  // imgLink: HTMLAnchorElement,
+  // backgroundImgContainer: HTMLElement,
+  // tryItLink: HTMLAnchorElement,
+  // gitHubLink: HTMLAnchorElement,
+  // rowContainer: HTMLElement
 ): void {
   if (receivedRepo.html_url === "https://github.com/Ayngie/assignment-API-graphQL_express-HiddenGems") {
     title.innerHTML = "Hidden Gems API";
     description.innerHTML = "A graphQL API + html/scss/js. Basically an updated todo-list.";
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "hiddenGemsImg";
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
   }
 
   if (receivedRepo.html_url === "https://github.com/Ayngie/assignment-API-graphQL_standalone-Webshop") {
@@ -23,7 +23,7 @@ export function modifyIndividualProject(
     description.innerHTML = "A graphQL apollo server API for a webshop shoppingcart.";
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "graphqlWebshopImg";
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
   }
 
   if (receivedRepo.html_url === "https://github.com/Ayngie/assignment-API-mongoDB_RestAPI-Webshop") {
@@ -31,7 +31,7 @@ export function modifyIndividualProject(
     description.innerHTML = "A MongoDb Apollo-server REST-API for a webshop shoppingcart.";
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "mongoDbWebshopImg";
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
   }
 
   if (receivedRepo.html_url === "https://github.com/Ayngie/gym-review-site-REST-API") {
@@ -39,7 +39,7 @@ export function modifyIndividualProject(
     description.innerHTML = "An SQLite express REST-API.";
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "gymReviewAppImg";
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
   }
 
   if (receivedRepo.html_url === "https://github.com/Ayngie/assignment-html_css-Ready-Design") {
@@ -48,7 +48,7 @@ export function modifyIndividualProject(
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "readyDesignImg";
     // tryItLink.setAttribute("href", "https://angelicareutersward.se/ReadyDesign/index.html");
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
     // rowContainer.appendChild(tryItLink);
   }
 
@@ -57,7 +57,7 @@ export function modifyIndividualProject(
     description.innerHTML = "An early and very basic html/scss/JS ToDo app.";
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "toDoImg";
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
   }
 
   if (receivedRepo.html_url === "https://github.com/Ayngie/assignment-typescript-Webshop-VackraMaltiderAB") {
@@ -66,7 +66,7 @@ export function modifyIndividualProject(
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "vackraMaltiderImg";
     // tryItLink.setAttribute("href", "https://angelicareutersward.se/Webshop/index.html");
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
     // rowContainer.appendChild(tryItLink);
   }
 
@@ -75,14 +75,14 @@ export function modifyIndividualProject(
     description.innerHTML = "Unit-/integrationtest/E2E";
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "testingCodeImg";
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
   }
 
   if (receivedRepo.html_url === "https://github.com/Ayngie/portfolio2.0") {
-    title.innerHTML = "My Portfolio";
+    title.innerHTML = "Portfolio 2.0";
     description.innerHTML = "Vite app with html/scss/TS";
     // imgLink.href = receivedRepo.html_url;
     // backgroundImgContainer.id = "portfolioImg";
-    gitHubLink.href = receivedRepo.html_url;
+    // gitHubLink.href = receivedRepo.html_url;
   }
 }
