cc3bca230882625e7e469a34a167d67a9ef9044a
Angie <71646930+Ayngie@users.noreply.github.com>
2024-04-30 16:19:35 +0200

    Created div and p's to structure paragraphs - createHTMLForProjectsContainer.ts

--- src/ts/functions/projectsContainer/createHTMLForProjectsContainer.ts
+++ src/ts/functions/projectsContainer/createHTMLForProjectsContainer.ts
@@ -3,9 +3,14 @@ export function createHTMLForProjectsContainer(): void {
 
   const heading = document.createElement("h4");
   heading.innerHTML = "My projects";
-  const description = document.createElement("p");
-  description.innerHTML = "Here are some of my projects from my time at Medieinstitutet Stockholm. Click the title to view the repo on GitHub!";
+  const description = document.createElement("div");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  p1.innerHTML = "Here are some of my projects from my time at Medieinstitutet Stockholm.";
+  p2.innerHTML = "Click the title to view the repo on GitHub!";
 
+  description.appendChild(p1);
+  description.appendChild(p2);
   container.appendChild(heading);
   container.appendChild(description);
 }
