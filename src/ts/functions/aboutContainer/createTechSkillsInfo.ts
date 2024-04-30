ba352394ae96030f40013a52b31cc5c7cdc9775f
Angie <71646930+Ayngie@users.noreply.github.com>
2024-04-30 16:32:48 +0200

    Updated skills in createTechSkillsInfo.ts

--- src/ts/functions/aboutContainer/createTechSkillsInfo.ts
+++ src/ts/functions/aboutContainer/createTechSkillsInfo.ts
@@ -6,7 +6,7 @@ export function createTechSkillsInfo(techSkills: HTMLElement): void {
   heading.innerHTML = "Technical Skills";
 
   const description = document.createElement("p");
-  description.innerHTML = `<p>Skills I will have aquired by the time of my LIA are:
+  description.innerHTML = `<p>Technical skills I have acquired (so far):
       </p>`;
   description.id = "techSkillsComment";
 
@@ -17,16 +17,26 @@ export function createTechSkillsInfo(techSkills: HTMLElement): void {
       <i class="devicon-javascript-plain">JavaScript </i>
       <i class="devicon-react-original">React.js </i>
       <i class="devicon-vuejs-plain">Vue.js </i>
+      <span>Next.js </span>
+      <span>Stencil.js </span>
+      <span>Storybook </span>
+      <i class="devicon-nodejs-plain">Node.js </i>
+      <span>CI/CD </span>
+      <span>DevOps </span>
+      <span>SwaggerHub </span>
+      <span>Postman </span>
+      <span>Agile Development </span>
       <i class="devicon-html5-plain-wordmark">HTML5 </i>
       <i class="devicon-sass-original">SCSS </i>
       <i class="devicon-css3-plain">CSS3 </i>
       <i class="devicon-bootstrap-plain">Bootstrap </i>
+      <span>Vite </span>
       <span>Parcel.js </span>
       <i class="devicon-jest-plain">Jest</i>
       <span>Cypress </span>
       <i class="devicon-git-plain">Git </i>
       <i class="devicon-github-original">GitHub </i>
-      <i class="devicon-nodejs-plain">Node.js </i>
+      <span>BitBucket </span>
       <i class="devicon-graphql-plain">GraphQL </i>
       <span>REST-Api </span>
       <i class="devicon-mongodb-plain">MongoDB </i>
