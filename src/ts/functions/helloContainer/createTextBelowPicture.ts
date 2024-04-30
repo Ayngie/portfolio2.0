63c08bb2f9b55f965226467f720593ab99118260
Angie <71646930+Ayngie@users.noreply.github.com>
2024-04-30 15:46:45 +0200

    Update about info in createTextBelowPicture.ts

--- src/ts/functions/helloContainer/createTextBelowPicture.ts
+++ src/ts/functions/helloContainer/createTextBelowPicture.ts
@@ -2,13 +2,13 @@ export function createTextBelowPicture(textBelowPicture: HTMLDivElement): HTMLEl
   textBelowPicture.id = "helloAboutParagraphs";
   const paragraph1 = document.createElement("p");
   paragraph1.innerHTML =
-    "Who am I? I'm an Osteopath turned Front End Developer. After a decade of working in manual medicine, I have discovered a new passion in frontend development. I never knew how much fun it was!";
+    "Who am I? I'm an Osteopath turned Front End Developer. After a decade of working in manual medicine, I discovered a new passion in frontend development. I never knew how much fun it was!";
   const paragraph2 = document.createElement("p");
   paragraph2.innerHTML =
-    "Once I started learning more, I couldn't stop. Now I'm more than halfway through the first year at Medieinstitutet Sthlm, and we're searching for internships as we speak...";
+    "Once I started learning more, I couldn't stop. I enrolled at Medieinstitutet, Stockholm, and halfway through my first year I created this portfolio (with techniques: Vanilla JS, TypeScript, HTML, SCSS).";
   const paragraph3 = document.createElement("p");
   paragraph3.innerHTML =
-    "If you have an internship opening, feel free to contact me. I'm so excited to get started in the tech world!";
+    "I've since then advanced, gained knowledge in various frameworks and libraries, and spent two internships at PostNord Group IT learning heaps and developing more skills. This portfolio however lives on :) For more advanced techniques why don't you view my degree project about Gestational Diabetes (you can find it under my projects below). I'm so excited to get to join the tech world!";
 
   textBelowPicture.appendChild(paragraph1);
   textBelowPicture.appendChild(paragraph2);
