fc0b624b728ca185fdac63cc28ade1f408897482
Angie <71646930+Ayngie@users.noreply.github.com>
2024-04-30 16:22:38 +0200

    Update createHTMLForProjectsContainer.ts

--- src/ts/functions/projectsContainer/createHTMLForProjectsContainer.ts
+++ src/ts/functions/projectsContainer/createHTMLForProjectsContainer.ts
@@ -3,14 +3,12 @@ export function createHTMLForProjectsContainer(): void {
 
   const heading = document.createElement("h4");
   heading.innerHTML = "My projects";
-  const description = document.createElement("div");
-  const p1 = document.createElement("p");
-  const p2 = document.createElement("p");
-  p1.innerHTML = "Here are some of my projects from my time at Medieinstitutet Stockholm.";
-  p2.innerHTML = "Click the title to view the repo on GitHub!";
+  const descriptionP1 = document.createElement("p");
+  const descriptionP2 = document.createElement("p");
+  descriptionP1.innerHTML = "Here are some of my projects from my time at Medieinstitutet Stockholm.";
+  descriptionP2.innerHTML = "Click the title to view the repo on GitHub!";
 
-  description.appendChild(p1);
-  description.appendChild(p2);
   container.appendChild(heading);
-  container.appendChild(description);
+  container.appendChild(descriptionP1);
+  container.appendChild(descriptionP2);
 }
