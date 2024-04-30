0714170ae4b279534081dd05b979fd5fc0b22aa5
Angie <71646930+Ayngie@users.noreply.github.com>
2024-04-30 16:16:26 +0200

    Added text about GitHub linking - createHTMLForProjectsContainer.ts

--- src/ts/functions/projectsContainer/createHTMLForProjectsContainer.ts
+++ src/ts/functions/projectsContainer/createHTMLForProjectsContainer.ts
@@ -4,7 +4,7 @@ export function createHTMLForProjectsContainer(): void {
   const heading = document.createElement("h4");
   heading.innerHTML = "My projects";
   const description = document.createElement("p");
-  description.innerHTML = "Here are some of my projects from my time at Medieinstitutet Stockholm.";
+  description.innerHTML = "Here are some of my projects from my time at Medieinstitutet Stockholm. Click the title to view the repo on GitHub!";
 
   container.appendChild(heading);
   container.appendChild(description);
