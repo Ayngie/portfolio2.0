e9f957cf61b64df42c39743c956662949119ddac
Angie <71646930+Ayngie@users.noreply.github.com>
2025-01-23 13:25:33 +0100

    Update createHTMLForContactContainer.ts

--- src/ts/functions/contactContainer/createHTMLForContactContainer.ts
+++ src/ts/functions/contactContainer/createHTMLForContactContainer.ts
@@ -6,7 +6,7 @@ export function createHTMLForContactContainer(): void {
 
   const paragraph1 = document.createElement("p");
   paragraph1.innerHTML =
-    "Graduating in may of 2024! Feel free to contact me for any job openings!";
+    "Graduated summer of 2024! Feel free to contact me for any job openings!";
 
   const paragraph2 = document.createElement("p");
   paragraph2.innerHTML = "Click an icon to get in touch with me!";
